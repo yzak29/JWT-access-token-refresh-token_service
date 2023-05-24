bcff44863aecae4fc3d5395d9eeb5324584d5c95
yzak29 <zakariayasser1@gmail.com>
2023-05-24 11:35:02 +0200

    first commit

---
+++ src/test/java/org/ysr/securityservice/SecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ysr.securityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
